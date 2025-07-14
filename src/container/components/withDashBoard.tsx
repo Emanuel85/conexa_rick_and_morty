ba960b52f8be67f7a26ee6cc4a2758ebac60c249
Emanuel85 <emanuelquintanilla85@gmail.com>
2025-07-14 17:31:17 -0300

    fix: comentado await

--- src/container/components/withDashBoard.tsx
+++ src/container/components/withDashBoard.tsx
@@ -1,12 +1,18 @@
+import api from '@/service/apiRickAndMorty';
 import React from 'react';
 
 const withDashBoard = <P extends object>(
-  Components: React.ComponentType<P>
+    Components: React.ComponentType<P>
 ): React.FC<P> => {
-  return (props: P) => {
-    const action = {};
-    return <Components {...action} {...props} />;
-  };
+    return (props: P) => {
+
+        // const { results } = await api.page.getPage("22")
+
+
+
+        const action = {};
+        return <Components {...action} {...props} />;
+    };
 };
 
 export default withDashBoard;
